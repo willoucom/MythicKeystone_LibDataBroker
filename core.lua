5c15f15932e9f488292a4ada86b83b2b88537998
Wilfried JEANNIARD <willou.com@gmail.com>
2023-06-07 22:20:00 +0200

    Feat: add AddonCompartmentFrame icon

--- core.lua
+++ core.lua
@@ -3,23 +3,58 @@ local ADDON, Addon = ...
 local f = CreateFrame("frame")
 local lib = LibStub("LibMythicKeystone-1.0")
 if not lib then return end
-local dataobj = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject('LibMythicKeystone', {type='data source', label='MythicKeystone'})
 
 Addon.Mykey = {}
 Addon.AltKeys = {}
+
+local dataobj = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject('LibMythicKeystone', {
+    type = 'data source',
+    label = 'MythicKeystone',
+    text = "MythicKeystone",
+    icon = "Interface\\Icons\\Inv_relics_hourglass",
+    OnClick = function()
+        if PVEFrame:IsShown() then
+            HideUIPanel(PVEFrame)
+        else
+            PVEFrame_ShowFrame()
+        end
+    end,
+
+})
+
+AddonCompartmentFrame:RegisterAddon({
+    text = 'MythicKeystone_LDB',
+    icon = "Interface\\Icons\\Inv_relics_hourglass",
+    registerForAnyClick = true,
+    notCheckable = true,
+    func = function(btn)
+        if PVEFrame:IsShown() then
+            HideUIPanel(PVEFrame)
+        else
+            PVEFrame_ShowFrame()
+        end
+    end,
+    funcOnEnter = function()
+        GameTooltip:SetOwner(AddonCompartmentFrame, "ANCHOR_TOPRIGHT")
+        GameTooltip:SetPoint("TOPLEFT", AddonCompartmentFrame, "BOTTOMLEFT")
+        GameTooltip:ClearLines()
+        dataobj.OnTooltipShow(GameTooltip)
+        GameTooltip:Show()
+    end,
+})
 
 f:SetScript("OnUpdate", function(self, elap)
     Addon.Mykey = lib.getMyKeystone()
     Addon.AltKeys = lib.getAltsKeystone()
     Addon.GuildKeys = lib.getGuildKeystone()
     if Addon.Mykey["current_key"] > 0 then
-        local keystoneMapName = Addon.Mykey["current_key"] and C_ChallengeMode.GetMapUIInfo(Addon.Mykey["current_key"]) or " "
-        dataobj.text = Addon.Mykey["current_keylevel"] .. " ".. keystoneMapName
+        local keystoneMapName = Addon.Mykey["current_key"] and C_ChallengeMode.GetMapUIInfo(Addon.Mykey["current_key"]) or
+            " "
+        dataobj.text = Addon.Mykey["current_keylevel"] .. " " .. keystoneMapName
     else
         dataobj.text = ""
     end
 end)
-
 
 local function formatText(obj)
     local name = obj["name"] or ""
@@ -27,19 +62,19 @@ local function formatText(obj)
     local color = "|cFFFFFFF"
     if obj["class"] ~= "" then
         color = C_ClassColor.GetClassColor(obj["class"]):GenerateHexColorMarkup()
-        name = color..name.."|r"
+        name = color .. name .. "|r"
     end
-    
+
     local keylevel = obj["current_keylevel"]
 
-    return string.format("%5s %s",keylevel, name)
+    return string.format("%5s %s", keylevel, name)
 end
 
 local function tableGroupByKeyLevel(obj)
     local keys = {}
     for _, key in pairs(obj) do
         keys[key["current_key"]] = keys[key["current_key"]] or {}
-        local tmp = {key["fullname"] , key["current_keylevel"]}
+        local tmp = { key["fullname"], key["current_keylevel"] }
         tinsert(keys[key["current_key"]], tmp)
     end
 
@@ -62,7 +97,7 @@ function dataobj:OnTooltipShow()
             self:AddLine("  " .. keystoneMapName)
             for char in pairs(keys[keyid]) do
                 char = keys[keyid][char][1]
-                self:AddLine("  ".. formatText(Addon.AltKeys[char]))
+                self:AddLine("  " .. formatText(Addon.AltKeys[char]))
             end
         end
     end
@@ -76,11 +111,8 @@ function dataobj:OnTooltipShow()
             self:AddLine("  " .. keystoneMapName)
             for char in pairs(keys[keyid]) do
                 char = keys[keyid][char][1]
-                self:AddLine("  ".. formatText(Addon.GuildKeys[char]))
+                self:AddLine("  " .. formatText(Addon.GuildKeys[char]))
             end
         end
     end
-
-
 end
-
